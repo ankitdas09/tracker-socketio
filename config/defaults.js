07f69b2848c18ae55b2742ed9663eaaed9853945
Ankit <ankit.99d@gmail.com>
2022-11-12 02:06:37 +0530

    auth token added

--- config/defaults.js
+++ config/defaults.js
@@ -4,4 +4,5 @@ module.exports = {
 	PORT: process.env.PORT,
 	MONGO_URI: process.env.MONGO_URI,
 	JWT_SECRET: "hujkd098qoijken8923798yuihkj",
+	TOKEN: "IITG",
 };
--- controllers/socket/authUser.js
+++ controllers/socket/authUser.js
@@ -1,7 +1,13 @@
+const config = require("../../config/defaults");
+
 module.exports.authUser = (socket, next) => {
 	const _id = socket.handshake.auth.id;
-	console.log(_id);
-	const user = { id: _id };
+	const token = socket.handshake.auth.token;
+	const user = {};
+	if (token !== config.TOKEN) {
+		user = { id: _id };
+	}
+	console.log(user);
 	if (!user.id) {
 		return next();
 	}
