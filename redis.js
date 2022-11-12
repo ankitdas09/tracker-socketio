d4a081e818450334e83d05e11fe493a9afe0cc9f
Ankit <ankit.99d@gmail.com>
2022-11-12 05:53:51 +0530

    package fixed

--- redis.js
+++ redis.js
@@ -2,7 +2,11 @@ const Redis = require("ioredis");
 require("dotenv").config();
 
 const redisClient = process.env.REDIS_URL
-	? new Redis(process.env.REDIS_URL)
+	? new Redis({
+			host: process.env.REDIS_HOST,
+			port: process.env.REDIS_PORT,
+			password: process.env.REDIS_PASSWORD,
+	  })
 	: new Redis();
 
 module.exports = redisClient;
